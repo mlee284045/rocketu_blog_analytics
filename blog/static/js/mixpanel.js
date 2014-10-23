309e89cd49743d3bd9b1d9c9ec1a1662271f3ac8
Mike Lee <mike.seung.lee@gmail.com>
2014-10-23 15:01:58 -0700

    more mixpanel

--- blog/static/js/mixpanel.js
+++ blog/static/js/mixpanel.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
     $("#featuredBlog").click(function () {
         mixpanel.track("Blog Footer Clicked");
     });
+
+    $('.sidebarTag').click(function(e) {
+        mixpanel.track("Sidebar clicked", {
+            "name": e.target.data('name')
+        });
+    })
+
 });
